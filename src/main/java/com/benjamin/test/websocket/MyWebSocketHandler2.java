c059c8f2075c697094008ca0825fe367d89589de
benjamin <benjamin1990@163.com>
2015-12-04 13:36:33 +0800

    fix bugs

--- src/main/java/com/benjamin/test/websocket/MyWebSocketHandler2.java
+++ src/main/java/com/benjamin/test/websocket/MyWebSocketHandler2.java
@@ -1,13 +1,14 @@
 package com.benjamin.test.websocket;
 
 import com.benjamin.websocket.handler.HttpSessionClusterWebSocketHandler;
+import com.benjamin.websocket.handler.SendIdentityClusterWebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 /**
  * Created by benjamin on 11/27/14.
  */
-public class MyWebSocketHandler2 extends HttpSessionClusterWebSocketHandler {
+public class MyWebSocketHandler2 extends SendIdentityClusterWebSocketHandler {
   @Override
   public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
     super.handleMessage(session, message);
